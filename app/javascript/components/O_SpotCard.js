1bd63cf8649b3a68a715d0f5775b4859e29b39e7
Ponomareva Daria <dorothinein@gmail.com>
2022-04-02 02:43:04 +0300

    еще чуть стиля на авторизация/регистрацию

--- app/javascript/components/O_SpotCard.js
+++ app/javascript/components/O_SpotCard.js
@@ -2,9 +2,13 @@ import React from "react"
 import PropTypes from "prop-types"
 import img from '../../assets/images/A_logo.svg'
 import A_Saved_Btn from "./A_Saved_Btn";
+import image from '../../assets/images/A_SpotImage.png'
 class O_SpotCard extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      image: this.props.spot?.image.url ? this.props.spot.image.url : image,
+    }
   } 
   render () {
     return (
@@ -16,7 +20,7 @@ class O_SpotCard extends React.Component {
           {this.props.saved[0] ? <A_Saved_Btn fillColor={'#151921'}/> : <A_Saved_Btn fillColor={'#F8F5F1'}/>}
         <div 
         className="spot-image"
-        style={{backgroundImage: `url(${this.props.spot?.image.url ? this.props.spot.image.url : 'https://xsaramps.com/files/uploads/conflict.png'})`}} />
+        style={{backgroundImage: `url(${this.state.image})`}} />
         
           <div className="spot-card-bottom">
           <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' ,marginBottom: '10px'}}>
