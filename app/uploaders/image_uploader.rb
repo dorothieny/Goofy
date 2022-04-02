b0477bac6a00b013978116ecaeeda754a5ce19f2
Ponomareva Daria <dorothinein@gmail.com>
2022-04-02 03:59:15 +0300

    ночное рандеву

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fit: [545, 360]
+  process resize_to_fit: [950, 740]
   # def scale(width, height)
   #   # do something
   # end
